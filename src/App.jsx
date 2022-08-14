a30b78790476506ca4c8f27aa45581c3d3354b9c
Erhan Güngör <erhan@snapbytes.com>
2022-08-14 16:47:12 +0300

    create react app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Typography } from "antd";
+import React from "react";
+
+const { Title } = Typography;
+
+const App = () => {
+    return (
+        <div>
+            <Title>Home Assistant Frontend</Title>
+        </div>
+    );
+};
+
+export default App;
